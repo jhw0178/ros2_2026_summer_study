56e6d9dc366c09f35ed50eb7b22059fdb422d9d6
jhw0178 <jhw0178@gmail.com>
2026-07-23 17:33:26 +0900

    4일차 끝

---
+++ ros_ws/build/tf2_basic/prefix_override/sitecustomize.py
@@ -0,0 +1,4 @@
+import sys
+if sys.prefix == '/usr':
+    sys.real_prefix = sys.prefix
+    sys.prefix = sys.exec_prefix = '/home/jhw0178/ros2_2026_summer_study/ros_ws/install/tf2_basic'
---
+++ ros_ws/src/tf2_basic/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+package_name = 'tf2_basic'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='jhw0178',
+    maintainer_email='jhw0178@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            "static_turtle_tf2_broadcaster = tf2_basic.static_turtle_tf2_broadcaster:main",
+            "dynamic_turtle_tf2_broadcaster = tf2_basic.dynamic_turtle_tf2_broadcaster:main",
+        ],
+    },
+)
