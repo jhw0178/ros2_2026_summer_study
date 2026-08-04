7b4a8cf92808d7f3c82b5036ded116493740fae3
jhw0178 <jhw0178@gmail.com>
2026-08-04 11:18:49 +0900

    12일차 중간

---
+++ opencv_test/a09Re_event_draw.py
@@ -0,0 +1,41 @@
+import cv2
+import numpy as np
+import color
+
+
+def onMouse(event, x, y, flags, param):
+    img, option = param
+    if event == cv2.EVENT_LBUTTONDOWN:
+        cv2.circle(img, (x, y), 1, color.RED, 5)
+        print("마우스 버튼 클릭")
+        onMouse.old_x = x       # 함수 속성 변수 (c의 static 처럼 활용 가능)
+        onMouse.old_y = y
+    if flags == cv2.EVENT_FLAG_LBUTTON and event == cv2.EVENT_MOUSEMOVE:
+        print("드래그")
+        print(list(color.COLORS.values()))
+        cv2.line(img, (onMouse.old_x, onMouse.old_y), (x, y), list(color.COLORS.values())[option[0]], 2)  # option을 통해 선 색을 변경함
+        onMouse.old_x = x
+        onMouse.old_y = y
+    elif event == cv2.EVENT_MOUSEMOVE:
+        print("마우스 움직임")
+    cv2.imshow("canvas", img)
+    
+
+def main():
+    cv2.namedWindow("canvas")
+    img = np.zeros([300, 700, 3], dtype=np.uint8)
+    option = [0]
+    cv2.setMouseCallback("canvas", onMouse, (img, option))
+    
+    cv2.imshow("canvas", img)
+    while True:
+        key = cv2.waitKey(30)
+        if key == ord("q"):
+            break
+        if key == ord(" "):
+            option[0] += 1
+        if option[0] > 12:
+            option[0] = 0
+
+if __name__ == "__main__":
+    main()
--- opencv_test/argument_copy.py
+++
@@ -1,9 +0,0 @@
-def argu_a(a):
-    a += 1
-    
-def main():
-    a = 6
-    argu_a([a]) # 라스트로 넘기거나 객체로 
-    print(a)
-    
-main()
