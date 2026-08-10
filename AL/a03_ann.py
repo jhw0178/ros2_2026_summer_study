bb73da916e794935d3d9192a6acdde64527cc050
jhw0178 <jhw0178@gmail.com>
2026-08-10 10:18:25 +0900

    15일차 작업 (대용량 모델 파일 제외)

--- AL/a03_ann.py
+++ AL/a03_ann.py
@@ -3,6 +3,7 @@ import numpy as np
 
 np.set_printoptions(precision=2, suppress=True)
 
+
 def main():
     x_train = np.array([[0, 0], [0, 1], [1, 0], [1, 1]], dtype=np.float32)
     y_train = np.array([[0, 0, 0, 1]], dtype=np.float32).reshape((4, 1))
